88b46ed2f1ceaa731de3db1e27a201927a51683c
IzadoraFernandes <izadorasilvams@gmail.com>
2023-07-07 17:22:46 -0300

    iniciando login social com github

---
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+
+  /*callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  */
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
