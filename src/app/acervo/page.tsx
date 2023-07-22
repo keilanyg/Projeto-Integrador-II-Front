7c0b1ebc855e95e264d4f041ce6d3fc2de91ff53
keilanyg <keilanydes@gmail.com>
2023-07-22 12:01:00 -0300

    Listando livros na pagina Acervo

--- src/app/acervo/page.tsx
+++ src/app/acervo/page.tsx
@@ -5,18 +5,29 @@ import style from './style.module.css'
 import Rodape from "@/components/Rodape/index"
 import BannerAcervo from 'public/BannerAcervo.png'
 import Image from "next/image"
-import Livro from "@/components/Livro/index"
-import Botao from "@/components/Botao/index";
+import Estudante from 'public/Estudante.jpg'
+import Botao from "@/components/Botao/index"
+import { api } from "@/app/services/api";
+import { useState, useEffect } from "react";
 
-
+interface Livros {
+    id: number;
+    nome_livro: string;
+}
 export default function Acervo() {
+    const [livros, setLivros] = useState<Livros[]>([]);
+    const getLivros = async () => {
+        const { data } = await api.get('livro/')
+        setLivros(data)
+    }
+    useEffect(() => {
+        getLivros();
+    }, []);
     return (
         <>
             <BarraNavegacao />
             <div className={style.body}>
-
                 <Image className={style.imagembanner} src={BannerAcervo} alt="BannerAcervo" />
-
                 <div className="row" id="camposfiltrar" style={{ color: "#4C3228", display: "flex", alignItems: "center", justifyContent: "space-evenly", margin: "0 200px", position: "relative" }}>
                     <div >
                         <input className=" mt-2 w-80 rounded-lg border border-gray-200 bg-white py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" style={{ border: "1px solid #8c5c3d", width: "100%" }} type="text" name="nome" placeholder="Nome " />
@@ -36,11 +47,22 @@ export default function Acervo() {
                         <Botao>Filtrar</Botao>
                     </div>
                 </div>
-
                 <div className={style.livro}>
-                    <Livro />
-                    <Livro />
-
+                    <div className={style.infolivro}>
+                        <ul>
+                            {livros.map(({ id, nome_livro }) => (
+                                <li key={id} style={{ display: "flex", margin: "20px 400px 20px 10px", justifyContent: "space-between", flexDirection: "column" }}>
+                                    <div>
+                                        <br /><Image className={style.imagemlivro} src={Estudante} alt="Estudante" />
+                                    </div>
+                                    <div className={style.titulo}>
+                                        <p>{nome_livro}</p>
+                                        <Botao>Acessar</Botao>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
             <Rodape />
