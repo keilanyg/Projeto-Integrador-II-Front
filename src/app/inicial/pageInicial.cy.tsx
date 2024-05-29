3937c2dedf5f94ecf8c334f55418f1c07e3918ee
keilany <keilanydes@gmail.com>
2024-05-29 16:19:53 -0300

    Resolução de Conflitos

--- src/app/inicial/pageInicial.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Inicial from './page'
-
-describe('<Inicial />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Inicial />)
-  })
-})
