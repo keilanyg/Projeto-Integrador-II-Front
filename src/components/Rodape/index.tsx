65cbc605adaf4980b6c8f55630d5b300db1b2a4b
keilany <keilanydes@gmail.com>
2024-06-20 13:26:04 -0300

    Tela inicial

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image"
-import Logo from 'public/Logo.png'
+import LogoR from 'public/LogoR.png'
 import style from './style.module.css'
 
 export default function Rodape() {
     return (
-        <footer>
+        <footer className={style.footer}>
             <div className={style.secaologo}>
-                <Image src={Logo} width={150} height={150} alt="Logo" />
-                <div>
-                    <h3>Serviços:</h3>
-                </div>
+                <Image src={LogoR} width={140} height={140} alt="Logo" />
                 <div>
                     <p>Empréstimos</p>
                 </div>
                 <div>
-                    <p>Consultas de Acervo</p>
+                    <p>Consultas de obras</p>
                 </div>
                 <div>
-                    <p>Cadastro de Livros</p>
+                    <p>Controle de Acervo</p>
                 </div>
             </div>
-        </footer>)
+        </footer>
+        )
 }
