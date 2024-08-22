d362ddbfa3dd2f3be0c9a1864cfbdb14b739ba07
keilanyg <keilanygabriel@gmail.com>
2024-08-22 12:43:29 -0300

    Adicionando pesquisa na listagem de Editora

--- src/components/ConteudoDasGuias/Editora/index.tsx
+++ src/components/ConteudoDasGuias/Editora/index.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import style from './style.module.css'
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { api } from "@/app/services/api";
 import Botao from "@/components/Botao/index";
 
-
-/*Menssagens*/
+/*Mensagens*/
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 interface Editora {
   id: number;
@@ -60,7 +57,8 @@ export default function ConteudoEditora() {
   const [nomeeditora, setNomeeditora] = useState("");
   const [editando, setEditando] = useState(false);
   const [editoraId, setEditoraId] = useState<number | null>(null);
-  
+  const [pesquisa, setPesquisa] = useState(""); // Estado para armazenar o termo de pesquisa
+
   const getEditora = async () => {
     const { data } = await api.get('editora/')
     setEditora(data)
@@ -70,10 +68,9 @@ export default function ConteudoEditora() {
     getEditora();
   }, []);
 
-
-  const postNameEditora = async (e) => {
+  const postNameEditora = async (e: React.FormEvent) => {
     e.preventDefault();
-    const newEditora = { nome_editora:nomeeditora };
+    const newEditora = { nome_editora: nomeeditora };
 
     if (editando && editoraId) {
       // Atualiza a editora existente
@@ -94,7 +91,7 @@ export default function ConteudoEditora() {
   const deleteEditora = async (id: number) => {
     await api.delete(`editora/${id}/`)
     notifyDelete();
-    getEditora()
+    getEditora();
   };
 
   const editEditora = (id: number, nome: string) => {
@@ -103,18 +100,45 @@ export default function ConteudoEditora() {
     setEditando(true);
   };
 
+  const editorasFiltradas = editora.filter(e =>
+    e.nome_editora.toLowerCase().includes(pesquisa.toLowerCase())
+  ); // Filtra a lista de editoras
+
   return (
     <>
+      <ToastContainer />
       <form onSubmit={postNameEditora}>
-        <label className=" text-sm text-gray-500 dark:text-gray-500" style={{ color: "#8c5c3d" }}>Nome da Editora</label><br />
-        <input type="text" value={nomeeditora} onChange={(e) => setNomeeditora(e.target.value)} className=" mt-2 w-80 rounded-lg border border-gray-200 bg-white py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" style={{ border: "1px solid #8c5c3d" }} />
-        <br /><br />
-        <Botao type="submit" >Salvar</Botao>
+        <label className="text-sm text-gray-500 dark:text-gray-500" style={{ color: "#8c5c3d" }}>
+          Nome da Editora
+        </label>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '18px' }}>
+          <div>
+            <input
+              type="text"
+              value={nomeeditora}
+              onChange={(e) => setNomeeditora(e.target.value)}
+              className="mt-2 w-80 rounded-lg border border-gray-200 bg-white py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+              style={{ border: "1px solid #8c5c3d" }}
+            />
+          </div>
+          <div>
+            <Botao type="submit">{editando ? "Atualizar" : "Salvar"}</Botao>
+          </div>
+        </div>
       </form>
 
       <div>
+        <br/>
+        <input
+          type="text"
+          placeholder="Pesquisar por nome..."
+          value={pesquisa}
+          onChange={(e) => setPesquisa(e.target.value)}
+          className="mt-2 w-80 rounded-lg border border-gray-200 bg-white py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+          style={{ border: "1px solid #8c5c3d" }}
+        />
         <ul>
-          {editora.map(({ id, nome_editora }) => (
+          {editorasFiltradas.map(({ id, nome_editora }) => (
             <li key={id} className={style.li}>
               <div>
                 {nome_editora}
@@ -125,7 +149,6 @@ export default function ConteudoEditora() {
               </div>
             </li>
           ))}
-
         </ul>
       </div>
     </>
